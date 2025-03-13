478ac21f4929d1f3a2e88db93a7f7683c575faed
shewagrk <shewagsurya02@gmail.com>
2025-03-13 21:15:19 +0530

    Started-Appending

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let addItem = document.getElementById('addItem');
+let inputText = document.querySelector('input').value;
 let groceryAppend = document.getElementById('groceryAppend');
 
 addItem.addEventListener('click', function(){
-    let ulList = document.createElement("ul");
-    let liList = document.createElement("li");
-    document.getElementById('grocery').v.append(liList);
-
-    ulList.append(liList);
-    groceryAppend.append(ulList);
+    let ul = document.createElement("ul");
+    let li = document.createElement("li");
+    // li.appendChild(inputText);
+    groceryAppend.appendChild(ul);
+    ul.appendChild(li);    
 })
